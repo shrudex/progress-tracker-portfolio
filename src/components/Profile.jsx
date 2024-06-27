ca2485b6e5f025e35e09f3838ff5b6f2ee40da04
shrudex <codeshubh@gmail.com>
2024-06-27 11:16:22 +0530

    edit profile page ui fixed

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -133,6 +133,25 @@ const Profile = () => {
 							</div>
 							<div className="sm:col-span-full">
 								<label
+									htmlFor="name"
+									className="block text-sm font-medium leading-6 text-gray-900"
+								>
+									Full Name
+								</label>
+								<div>
+									<input
+										id="name"
+										name="name"
+										type="text"
+										autoComplete="name"
+										className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-purple-700 sm:text-sm sm:leading-6"
+										value={userData.name || ""}
+										onChange={handleChange}
+									/>
+								</div>
+							</div>
+							<div className="sm:col-span-full">
+								<label
 									htmlFor="email"
 									className="block text-sm font-medium leading-6 text-gray-900"
 								>
@@ -204,7 +223,7 @@ const Profile = () => {
 											<img
 												src={icon}
 												alt="Profile icon"
-												className="h-12 w-fit rounded-full"
+												className="h-12 w-fit hover:shadow-2xl	 rounded-full hover:h-20 transition-all duration-300 ease-in-out"
 											/>
 										</button>
 									))}
